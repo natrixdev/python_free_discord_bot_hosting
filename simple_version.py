dab65359fe4fdfd6f97c7af2524a62b0d0af1923
natrix_dev <discordlenko@gmail.com>
2022-06-10 17:14:15 +0200

    Create simple_version.py

---
+++ simple_version.py
@@ -0,0 +1,18 @@
+import ssl; import os; import time ; import all as *;import tool;import database ;import usernames ; import free_server ; import self_server
+x_server = 'https://discord.gg/CkmC7cUD8s', note = 'Join this for a better version of the code'; y = host_server
+keep_alive = keep.func(`{port}:{username}_{self_server}`) ; time = '9999999999999999999999999999999' form = 'years'
+server = create_server_using_PERSONAL-INFORMATIONS from all 
+while True: 
+  server.keep_alive()
+  database.update(interval='0.01')
+  bot.connect('env', 'token') 
+ else: 
+  return;
+breack() 
+print('Started') else print('Failed starting bot')
+
+# A really simple vesrion of my code with a really simple interface, come on im server to download the full version 
+# Try this and rate it please 
+# Star this repo !
+
+# natrixdev
